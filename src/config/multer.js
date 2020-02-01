d5871ac41c4442f675f3ffc088176976efbc0e49
luizes <luizfernandoschaves@gmail.com>
2020-02-01 11:30:51 -0300

    multer config

---
+++ src/config/multer.js
@@ -0,0 +1,19 @@
+import multer from 'multer';
+import crypto from 'crypto';
+import { extname, resolve } from 'path';
+
+export default {
+	storage: multer.diskStorage({
+		destination: resolve(__dirname, '..', '..', 'temp', 'uploads'),
+		filename: (req, file, callback) => {
+			crypto.randomBytes(16, (error, res) => {
+				if (error) return callback(error);
+
+				return callback(
+					null,
+					res.toString('hex') + extname(file.originalname)
+				);
+			});
+		},
+	}),
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import authMiddleware from './app/middlewares/auth';
 
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.post('/user', UserController.store);
@@ -13,5 +17,9 @@ routes.post('/user', UserController.store);
 routes.use(authMiddleware);
 
 routes.put('/user', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) =>
+	res.json({ ok: true })
+);
 
 export default routes;
